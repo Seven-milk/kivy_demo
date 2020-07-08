4a78ee194340991d2264c7b7bb1569e26bf98b85
Seven <58417552+Seven-milk@users.noreply.github.com>
2020-07-08 18:00:01 +0800

    add start.text

--- lijia.py
+++ lijia.py
@@ -2,11 +2,16 @@
 # author: "Xudong Zheng" 
 # email: Z786909151@163.com
 import datetime
-start = datetime.date(2020, 6, 12)
+
+# start = datetime.date(2020, 6, 12)
+with open('start.text', 'r') as f:
+    f_read = f.read().split(',')
+    start = datetime.date(int(f_read[0]), int(f_read[1]), int(f_read[2]))
+
 time_delta = datetime.timedelta(days=28)
 now = datetime.date.today()
 dif = now - start
-date = start + ((dif//time_delta) + 1) * time_delta
+date = start + ((dif // time_delta) + 1) * time_delta
 print("下次例假是：{}".format(date))
 # Todo 设计APP
 # Todo 传入修改start日期功能
